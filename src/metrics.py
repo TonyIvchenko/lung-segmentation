25083c728936a1798377b3b10e29b850448e7e13
Tony Ivchenko <toxa.ivchenko@gmail.com>
2021-03-30 10:29:51 -0800

    Refactor segmentation metrics for safer tensor flattening and dtype handling

--- src/metrics.py
+++ src/metrics.py
@@ -1,33 +1,29 @@
 import torch
 
 
-def jaccard(y_true, y_pred):
-    """ Jaccard a.k.a IoU score for batch of images
-    """
-    
-    num = y_true.size(0)
-    eps = 1e-7
-    
-    y_true_flat = y_true.view(num, -1)
-    y_pred_flat = y_pred.view(num, -1)
-    intersection = (y_true_flat * y_pred_flat).sum(1)
-    union = ((y_true_flat + y_pred_flat) > 0.0).float().sum(1)
-    
-    score = (intersection) / (union + eps)
-    score = score.sum() / num
-    return score
-    
+def _flatten_batch(y_true, y_pred):
+    if y_true.size(0) != y_pred.size(0):
+        raise ValueError("y_true and y_pred must have the same batch size")
 
-def dice(y_true, y_pred):
-    """ Dice a.k.a f1 score for batch of images
-    """
     num = y_true.size(0)
-    eps = 1e-7
-    
-    y_true_flat = y_true.view(num, -1)
-    y_pred_flat = y_pred.view(num, -1)
-    intersection = (y_true_flat * y_pred_flat).sum(1)
-    
-    score =  (2 * intersection) / (y_true_flat.sum(1) + y_pred_flat.sum(1) + eps)
-    score = score.sum() / num
-    return score
+    y_true_flat = y_true.float().reshape(num, -1)
+    y_pred_flat = y_pred.float().reshape(num, -1)
+    return num, y_true_flat, y_pred_flat
+
+
+def jaccard(y_true, y_pred, eps=1e-7):
+    """Jaccard a.k.a IoU score for a batch of masks."""
+    _, y_true_flat, y_pred_flat = _flatten_batch(y_true, y_pred)
+    intersection = (y_true_flat * y_pred_flat).sum(dim=1)
+    union = ((y_true_flat + y_pred_flat) > 0.0).float().sum(dim=1)
+    score = intersection / (union + eps)
+    return score.mean()
+
+
+def dice(y_true, y_pred, eps=1e-7):
+    """Dice a.k.a F1 score for a batch of masks."""
+    _, y_true_flat, y_pred_flat = _flatten_batch(y_true, y_pred)
+    intersection = (y_true_flat * y_pred_flat).sum(dim=1)
+    denominator = y_true_flat.sum(dim=1) + y_pred_flat.sum(dim=1) + eps
+    score = (2 * intersection) / denominator
+    return score.mean()
