7cdec03c094095e1cff6dd0d3e33610fb6488294
Tony Ivchenko <toxa.ivchenko@gmail.com>
2021-07-07 12:04:49 -0800

    Allow evaluation CLI to auto-detect model type from checkpoint

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -47,7 +47,7 @@ def parse_args():
     parser.add_argument("--batch-size", type=int, default=4)
     parser.add_argument("--num-workers", type=int, default=0)
     parser.add_argument("--image-size", type=int, default=512)
-    parser.add_argument("--model", choices=["unet", "pretrained-unet"], default="pretrained-unet")
+    parser.add_argument("--model", choices=["auto", "unet", "pretrained-unet"], default="auto")
     parser.add_argument("--cpu", action="store_true")
     return parser.parse_args()
 
@@ -56,12 +56,12 @@ def main():
     args = parse_args()
 
     device = resolve_device(prefer_cuda=not args.cpu)
+    model_name = None if args.model == "auto" else args.model
     model, _ = load_checkpoint(
         path=args.checkpoint,
-        model_name=args.model,
+        model_name=model_name,
         device=device,
-        batch_norm=args.model == "pretrained-unet",
-        upscale_mode="bilinear",
+        batch_norm=False,
     )
 
     with open(args.splits, "rb") as split_file:
