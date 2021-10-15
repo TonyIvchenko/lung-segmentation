9d7274eaecd6351090d6a7571b6f9d8315faf921
Tony Ivchenko <toxa.ivchenko@gmail.com>
2021-10-15 14:19:27 -0800

    Simplify train-arg tests using scripts package import path

--- tests/test_train_args.py
+++ tests/test_train_args.py
@@ -3,16 +3,7 @@ import pytest
 
 pytest.importorskip("torch")
 
-import importlib.util
-from pathlib import Path
-
-
-_SPEC = importlib.util.spec_from_file_location(
-    "train_cli",
-    Path(__file__).resolve().parents[1] / "scripts" / "train.py",
-)
-train = importlib.util.module_from_spec(_SPEC)
-_SPEC.loader.exec_module(train)
+from scripts import train
 
 
 def test_pretrained_unet_defaults_to_pretrained_encoder(monkeypatch):
