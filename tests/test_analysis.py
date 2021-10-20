934ff982665909a89078fa1193998985bab6fd33
Tony Ivchenko <toxa.ivchenko@gmail.com>
2021-10-19 17:43:34 -0800

    Extend analysis tests for count-based precision/recall helper

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -3,7 +3,7 @@ import pytest
 
 torch = pytest.importorskip("torch")
 
-from src.analysis import confusion_counts, precision_recall_f1
+from src.analysis import confusion_counts, precision_recall_f1, precision_recall_f1_from_counts
 
 
 def test_confusion_counts_binary_masks():
@@ -22,3 +22,9 @@ def test_precision_recall_f1_values():
     assert metrics["precision"] == pytest.approx(0.5)
     assert metrics["recall"] == pytest.approx(0.5)
     assert metrics["f1"] == pytest.approx(0.5)
+
+
+def test_precision_recall_f1_from_counts_values():
+    metrics = precision_recall_f1_from_counts(tp=3, fp=1, fn=2)
+    assert metrics["precision"] == pytest.approx(0.75)
+    assert metrics["recall"] == pytest.approx(0.6)
