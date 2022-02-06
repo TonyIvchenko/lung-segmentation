d17da07514cb034e4867880802f7b73c14056bb9
Tony Ivchenko <toxa.ivchenko@gmail.com>
2022-02-06 12:51:03 -0800

    feat: make split generation configurable for mask naming conventions

--- scripts/make_splits.py
+++ scripts/make_splits.py
@@ -3,8 +3,6 @@ import argparse
 import pickle
 from pathlib import Path
 
-from sklearn.model_selection import train_test_split
-
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Create train/val/test splits for lung segmentation")
@@ -13,22 +11,46 @@ def parse_args():
     parser.add_argument("--test-size", type=float, default=0.2)
     parser.add_argument("--val-size", type=float, default=0.1)
     parser.add_argument("--seed", type=int, default=42)
+    parser.add_argument("--mask-suffix", default="_mask")
+    parser.add_argument("--image-ext", default=".png")
+    parser.add_argument("--mask-ext", default=".png")
+    parser.add_argument(
+        "--strict-pairs",
+        action="store_true",
+        help="Fail if any mask does not have a matching origin image.",
+    )
     return parser.parse_args()
+
+
+def origin_name_from_mask(mask_stem, mask_suffix):
+    if not mask_suffix:
+        return mask_stem
+    if not mask_stem.endswith(mask_suffix):
+        raise ValueError(
+            f"mask stem '{mask_stem}' does not end with configured suffix '{mask_suffix}'"
+        )
+    return mask_stem[: -len(mask_suffix)]
 
 
 def main():
     args = parse_args()
+    from sklearn.model_selection import train_test_split
 
     origins_folder = args.data_folder / "images"
     masks_folder = args.data_folder / "masks"
 
-    origins = {path.stem for path in origins_folder.glob("*.png")}
-    masks = sorted(path.stem for path in masks_folder.glob("*.png"))
-    origin_mask_pairs = [(mask.replace("_mask", ""), mask) for mask in masks]
+    origins = {path.stem for path in origins_folder.glob(f"*{args.image_ext}")}
+    masks = sorted(path.stem for path in masks_folder.glob(f"*{args.mask_ext}"))
+    origin_mask_pairs = [
+        (origin_name_from_mask(mask_stem, args.mask_suffix), mask_stem)
+        for mask_stem in masks
+    ]
 
     filtered_pairs = [pair for pair in origin_mask_pairs if pair[0] in origins]
     if len(filtered_pairs) != len(origin_mask_pairs):
         missing = len(origin_mask_pairs) - len(filtered_pairs)
+        if args.strict_pairs:
+            raise ValueError(f"found {missing} masks without matching origin image")
         print(f"warning: dropped {missing} masks with missing origin image")
 
     train_pairs, test_pairs = train_test_split(
---
+++ tests/test_make_splits.py
@@ -0,0 +1,16 @@
+import pytest
+
+from scripts import make_splits
+
+
+def test_origin_name_from_mask_with_default_suffix():
+    assert make_splits.origin_name_from_mask("case001_mask", "_mask") == "case001"
+
+
+def test_origin_name_from_mask_with_empty_suffix():
+    assert make_splits.origin_name_from_mask("case001", "") == "case001"
+
+
+def test_origin_name_from_mask_raises_for_suffix_mismatch():
+    with pytest.raises(ValueError):
+        make_splits.origin_name_from_mask("case001", "_mask")
